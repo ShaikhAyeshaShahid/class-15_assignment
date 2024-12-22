1db770b8e6fc42fa44e3fd3b6d0e82e64e381008
Shaikh Ayesha Shahid <85065617+ShaikhAyeshaShahid@users.noreply.github.com>
2024-12-22 22:55:22 +0500

    Update page.tsx

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,26 +1,42 @@
-const dynamicUser = async (props: any) => {
-    console.log("Props:", props);
-
-    const fetchURL = `https://jsonplaceholder.typicode.com/users/${props.params.id}`;
-    console.log("Fetch URL:", fetchURL);
-
-    const res = await fetch(fetchURL, { cache: "no-store" });
-    if (!res.ok) {
-        console.error("Failed to fetch user data:", res.statusText);
-        return <div>User not found.</div>;
-    }
-
-    const response = await res.json();
-    console.log("API Response (User Data):", response);
-
+const dynamicUser = async ({ params }: { params: { id: string } }) => {
+    console.log(params)
+    const url = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
+    const response = await url.json()
+    console.log("Single User", response)
     return (
-        <div>
-            <h1>
-                Dynamic Route <br />
-                {response.title || "Name not found"}
-            </h1>
-        </div>
-    );
-};
-
+        <h1 className="text-7xl">
+            Dynammic Route <br /> <br />
+            {response.name}
+        </h1>
+    )
+}
 export default dynamicUser;
+
+
+
+// const dynamicUser = async (props: any) => {
+//     console.log("Props:", props);
+
+//     const fetchURL = `https://jsonplaceholder.typicode.com/users/${props.params.id}`;
+//     console.log("Fetch URL:", fetchURL);
+
+//     const res = await fetch(fetchURL, { cache: "no-store" });
+//     if (!res.ok) {
+//         console.error("Failed to fetch user data:", res.statusText);
+//         return <div>User not found.</div>;
+//     }
+
+//     const response = await res.json();
+//     console.log("API Response (User Data):", response);
+
+//     return (
+//         <div>
+//             <h1>
+//                 Dynamic Route <br />
+//                 {response.name || "Name not found"}
+//             </h1>
+//         </div>
+//     );
+// };
+
+// export default dynamicUser;
