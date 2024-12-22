912eec936ed8911b98ccc05f2bea33c6dfbcd1cc
Shaikh Ayesha Shahid <85065617+ShaikhAyeshaShahid@users.noreply.github.com>
2024-12-22 23:02:28 +0500

    Update page.tsx

--- src/app/todos/[id]/page.tsx
+++ src/app/todos/[id]/page.tsx
@@ -1,15 +1,22 @@
 // src/app/todos/[id]/page.tsx
-const TodoDetails = async ({ params }: { params: { id: string } }) => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${params.id}`);
-    const todo = await res.json();
-    
-    return (
-      <div>
-        <h1>Todo: {todo.title}</h1>
-        <p>{todo.completed ? "Completed" : "Not completed"}</p>
-      </div>
-    );
+import { FC } from "react";
+
+interface PageProps {
+  params: {
+    id: string;
   };
-  
-  export default TodoDetails;
-  
+}
+
+const TodoDetails: FC<PageProps> = async ({ params }) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${params.id}`);
+  const todo = await res.json();
+
+  return (
+    <div>
+      <h1>Todo: {todo.title}</h1>
+      <p>{todo.completed ? "Completed" : "Not completed"}</p>
+    </div>
+  );
+};
+
+export default TodoDetails;
